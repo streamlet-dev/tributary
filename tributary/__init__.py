edf453e99e0fa9242562464305ff7e3b5344b893
Tim Paine <t.paine154@gmail.com>
2018-09-08 20:47:11 -0400

    pipeline working again

--- tributary/__init__.py
+++ tributary/__init__.py
@@ -1,9 +1,17 @@
 import time
+import signal
 from concurrent.futures import ThreadPoolExecutor, _base
-from concurrent.futures.thread import BrokenThreadPool, _shutdown, _WorkItem
+from concurrent.futures.thread import BrokenThreadPool, _WorkItem
+import concurrent.futures.thread as cft
 from functools import partial
 
 _EXECUTOR = ThreadPoolExecutor(max_workers=2)
+
+
+def signal_handler(signal, frame):
+    cft._shutdown = True
+    print('*************\n\n\n\nHERE\n\n\n\n\n****************')
+    raise Exception('Work Interrupted!')
 
 
 def timer(foo, args=None, kwargs=None, interval=1, repeat=0):
@@ -21,27 +29,6 @@ def timer(foo, args=None, kwargs=None, interval=1, repeat=0):
     return _repeater
 
 
-''' Python concurrent future
-    def submit(self, fn, *args, **kwargs):
-        with self._shutdown_lock:
-            if self._broken:
-                raise BrokenThreadPool(self._broken)
-
-            if self._shutdown:
-                raise RuntimeError('cannot schedule new futures after shutdown')
-            if _shutdown:
-                raise RuntimeError('cannot schedule new futures after'
-                                   'interpreter shutdown')
-
-            f = _base.Future()
-            w = _WorkItem(f, fn, args, kwargs)
-
-            self._work_queue.put(w)
-            self._adjust_thread_count()
-            return f
-'''
-
-
 def submit(fn, *args, **kwargs):
     self = _EXECUTOR
     with self._shutdown_lock:
@@ -50,7 +37,7 @@ def submit(fn, *args, **kwargs):
 
         if self._shutdown:
             raise RuntimeError('cannot schedule new futures after shutdown')
-        if _shutdown:
+        if cft._shutdown:
             raise RuntimeError('cannot schedule new futures after'
                                'interpreter shutdown')
 
@@ -62,13 +49,16 @@ def submit(fn, *args, **kwargs):
         return f
 
 
-def run_submit(fn, *args, **kwargs):
+def run_submit(fn, function_to_call, *args, **kwargs):
     f = submit(fn, *args, **kwargs)
-    return f.result()
+
+    if function_to_call:
+        f.add_done_callback(lambda fut: function_to_call(fut.result()) if fut.result() else None)
 
 
-def pipeline(foos, foo_callbacks, foo_kwargs=None, sleep=1, on_data=print):
+def pipeline(foos, foo_callbacks, foo_kwargs=None, on_data=print, on_data_kwargs=None):
     foo_kwargs = foo_kwargs or []
+    on_data_kwargs = on_data_kwargs or {}
 
     # organize args for functional pipeline
     assembled = []
@@ -79,19 +69,21 @@ def pipeline(foos, foo_callbacks, foo_kwargs=None, sleep=1, on_data=print):
 
     # assemble pipeline
     assembled.reverse()
-    lambdas = [on_data]
+    lambdas = [lambda d, f=on_data: run_submit(f, None, d, **on_data_kwargs)]
     for i, a in enumerate(assembled):
         foo, cb, kwargs = a
-        kwargs[cb] = lambdas[i]
+        function_to_call = lambdas[i]
+        kwargs[cb] = function_to_call
 
         if i != len(assembled)-1:
-            # lambdas.append(lambda d, kw=kwargs, f=foo: f(d, **kw))
-            lambdas.append(lambda d, kw=kwargs, f=foo: run_submit(f, d, **kw))
-            lambdas[-1].__name__ = 'lambda-%d' % i
+            lambdas.append(lambda d, kw=kwargs, f=foo: run_submit(f, function_to_call, d, **kw))
+            lambdas[-1].__name__ = foo.__name__
         else:
-            # lambdas.append(lambda kw=kwargs, f=foo: f(**kw))
-            lambdas.append(lambda kw=kwargs, f=foo: run_submit(f, **kw))
-            lambdas[-1].__name__ = 'lambda-%d' % i
+            lambdas.append(lambda kw=kwargs, f=foo: run_submit(f, function_to_call, **kw))
+            lambdas[-1].__name__ = foo.__name__
+
+    # ensure signal caught
+    signal.signal(signal.SIGINT, signal_handler)
 
     # return entrypoint
     return lambdas[-1]
