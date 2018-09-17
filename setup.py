496636e351d53b4cd880dc2299e04fd6d39f43e7
Tim Paine <t.paine154@gmail.com>
2018-09-17 15:22:58 -0400

    fixes #6, bump version

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 
 setup(
     name='tributary',
-    version='0.0.1',
+    version='0.0.2',
     description='Analytics library',
     long_description=long_description,
     url='https://github.com/timkpaine/tributary',
-    download_url='https://github.com/timkpaine/tributary/archive/v0.0.1.tar.gz',
+    download_url='https://github.com/timkpaine/tributary/archive/v0.0.2.tar.gz',
     author='Tim Paine',
     author_email='timothy.k.paine@gmail.com',
     license='Apache 2.0',
--- tributary/__init__.py
+++ tributary/__init__.py
@@ -1,2 +1,4 @@
 from .functional import pipeline, stop
 from .reactive import *
+
+__version__ = '0.0.2'
