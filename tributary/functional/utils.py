daac60845cc24f3a66d83853f67f21b34cd7e661
Tim Paine <t.paine154@gmail.com>
2018-09-19 23:12:06 -0400

    fixes #8

--- tributary/functional/utils.py
+++ tributary/functional/utils.py
@@ -3,5 +3,13 @@ def split(data, callback1, callback2):
     return (callback1(data), callback2(data))
 
 
+def map(data, *callbacks):
+    return (callback(data) for callback in callbacks)
+
+
 def merge(data1, data2, callback):
     return callback((data1, data2))
+
+
+def reduce(callback, *datas):
+    return callback((data for data in datas))
