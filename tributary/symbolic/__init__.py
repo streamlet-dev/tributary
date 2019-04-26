af93551ecbc5049b26b7bab83962b9e742be7c2e
Tim Paine <t.paine154@gmail.com>
2019-04-25 20:30:42 -0400

    fixes #36

--- tributary/symbolic/__init__.py
+++ tributary/symbolic/__init__.py
@@ -10,24 +10,51 @@ init_printing(use_unicode=True)
 
 
 def parse_expression(expr):
+    '''Parse string as sympy expression
+    Args:
+        expr (string): string to convert to sympy expression
+    '''
     return parse_expr(expr, transformations=(_st + (_ima,)))
 
 
 def graphviz(expr):
+    '''Plot sympy expression tree using graphviz
+    Args:
+        expr (sympy expression)
+    '''
+
     return Source(dotprint(expr))
 
 
 def traversal(expr):
+    '''Traverse sympy expression tree
+    Args:
+        expr (sympy expression)
+    '''
+
     return list(preorder_traversal(expr))
 
 
 def symbols(expr):
+    '''Get symbols used in sympy expression
+    Args:
+        expr (sympy expression)
+    '''
     return expr.free_symbols
 
 
-def construct_lazy(expr):
+def construct_lazy(expr, modules=None):
+    '''Construct Lazy tributary class from sympy expression
+
+    Args:
+        expr (sympy expression): A Sympy expression
+        modules (list): a list of modules to use for sympy's lambdify function
+    Returns:
+        tributary.lazy.BaseClass
+    '''
     syms = list(symbols(expr))
     names = [s.name for s in syms]
+    modules = modules or ["scipy", "numpy"]
 
     class Lazy(tl.BaseClass):
         def __init__(self, **kwargs):
@@ -35,7 +62,7 @@ def construct_lazy(expr):
                 setattr(self, n, self.node(name=n, default_or_starting_value=kwargs.get(n, None)))
 
             self._nodes = [getattr(self, n) for n in names]
-            self._function = lambdify(syms, expr)
+            self._function = lambdify(syms, expr, modules=modules)
             self._expr = expr
 
         @tl.node
