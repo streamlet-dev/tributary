a44b319aed5b0fc7e1b5d3de139bd1ac2b60888d
Tim Paine <t.paine154@gmail.com>
2019-06-09 21:16:14 -0400

    more tests

---
+++ tests/reactive/calculations/test_rolling_reactive.py
@@ -0,0 +1,27 @@
+import tributary as t
+
+
+class TestOps:
+    def test_count(self):
+        def foo():
+            yield 1
+            yield 2
+            yield 3
+
+        out = t.run(t.Count(foo))
+        assert len(out) == 3
+        assert out[-1] == 3
+        assert out[-2] == 2
+        assert out[-3] == 1
+
+    def test_sub(self):
+        def foo():
+            yield 1
+            yield 2
+            yield 3
+
+        out = t.run(t.Sum(foo))
+        assert len(out) == 3
+        assert out[-1] == 6
+        assert out[-2] == 3
+        assert out[-3] == 1
