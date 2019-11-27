7a3be938bb8540f68afaef90d6ef26e28db00bf1
Tim Paine <t.paine154@gmail.com>
2019-11-27 18:48:13 -0500

    adding sympy streaming example

--- tests/symbolic/test_symbolic.py
+++ tests/symbolic/test_symbolic.py
@@ -48,3 +48,19 @@ class TestConfig:
     def test_parse(self):
         from sympy.parsing.sympy_parser import parse_expr
         assert parse_expr('x^2') == ts.parse_expression('x^2')
+
+    def test_construct_streaming(self):
+        import tributary.symbolic as ts
+        import tributary.reactive as tr
+
+        expr = ts.parse_expression("10sin**2 x**2 + 3xyz + tan theta")
+        clz = ts.construct_streaming(expr)
+
+        def foo(*args):
+            for i in range(10):
+                yield i
+
+        x = clz(x=tr.Const(1), y=tr.Foo(foo), z=tr.Timer(1, repeat=1), theta=tr.Const(4))
+        x.pprint()
+        out = x.run()
+        assert len(out) == 1
