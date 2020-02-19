f1632523874e895f955283a799fe59e0a5fc457f
Tim Paine <t.paine154@gmail.com>
2020-02-19 21:56:52 +0000

    .

--- scripts/test_notebooks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for notebook in `ls examples/*.ipynb`; do
-    echo "python3 -m nbconvert --to=notebook --execute examples/$notebook"
-    IEX_TOKEN=Tpk_ecc89ddf30a611e9958142010a80043c python3 -m nbconvert --to=notebook --execute $notebook --stdout
-    # python3 -m nbconvert --to=notebook --execute $notebook > /dev/null 2>&1 || echo "failed: $notebook"
-done
