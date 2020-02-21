45dc4d1fb6ba722e6e8b18f9b2379f445e47e7ef
Tim Paine <t.paine154@gmail.com>
2020-02-21 08:30:11 -0500

    convert to functions

--- tributary/streaming/calculations/rolling.py
+++ tributary/streaming/calculations/rolling.py
@@ -1,107 +1,110 @@
 from ..base import Node
 
 
-class Count(Node):
-    '''Node to count inputs'''
+def Count(node):
+    '''Node to count inputs
+    
+    Args:
+        node (Node): input stream
+    '''
+    def foo(val):
+        ret._count += 1
+        return ret._count
 
-    def __init__(self, node):
-        self._count = 0
-
-        def foo(val):
-            self._count += 1
-            return self._count
-
-        super().__init__(foo=foo, foo_kwargs=None, name='Count', inputs=1)
-
-        node._downstream.append((self, 0))
-        self._upstream.append(node)
-
-
-class Max(Node):
-    '''Node to take rolling max of inputs'''
-
-    def __init__(self, node):
-        self._max = None
-
-        def foo(val):
-            self._max = max(self._max, val) if self._max is not None else val
-            return self._max
-
-        super().__init__(foo=foo, foo_kwargs=None, name='Max', inputs=1)
-
-        node._downstream.append((self, 0))
-        self._upstream.append(node)
+    ret = Node(foo=foo, foo_kwargs=None, name='Count', inputs=1)
+    ret._count = 0
+    node._downstream.append((ret, 0))
+    ret._upstream.append(node)
+    return ret
 
 
-class Min(Node):
-    '''Node to take rolling min of inputs'''
+def Max(node):
+    '''Node to take rolling max of inputs
+    
+    Args:
+        node (Node): input stream
+    '''
+    def foo(val):
+        ret._max = max(ret._max, val) if ret._max is not None else val
+        return ret._max
 
-    def __init__(self, node):
-        self._min = None
+    ret = Node(foo=foo, foo_kwargs=None, name='Max', inputs=1)
+    ret._max = None
 
-        def foo(val):
-            self._min = min(self._min, val) if self._min is not None else val
-            return self._min
-
-        super().__init__(foo=foo, foo_kwargs=None, name='Min', inputs=1)
-
-        node._downstream.append((self, 0))
-        self._upstream.append(node)
+    node._downstream.append((ret, 0))
+    ret._upstream.append(node)
+    return ret
 
 
-class Sum(Node):
+def Min(node):
+    '''Node to take rolling min of inputs
+    
+    Args:
+        node (Node): input stream
+    '''
+    def foo(val):
+        ret._min = min(ret._min, val) if ret._min is not None else val
+        return ret._min
+
+    ret = Node(foo=foo, foo_kwargs=None, name='Min', inputs=1)
+    ret._min = None
+    node._downstream.append((ret, 0))
+    ret._upstream.append(node)
+    return ret
+
+
+def Sum(node):
     '''Node to take rolling sum inputs
 
     If stream type is iterable, will do += sum(input). If input
     stream type is not iterable, will do += input.
+    
+    Args:
+        node (Node): input stream
     '''
+    def foo(val):
+        try:
+            # iterable, sum with sum function
+            iter(val)
+            ret._sum += sum(val)
+        except TypeError:
+            # not iterable, sum by value
+            ret._sum += val
+        return ret._sum
 
-    def __init__(self, node):
-        self._sum = 0
+    ret = Node(foo=foo, foo_kwargs=None, name='Sum', inputs=1)
+    ret._sum = 0
 
-        def foo(val):
-            try:
-                # iterable, sum with sum function
-                iter(val)
-                self._sum += sum(val)
-            except TypeError:
-                # not iterable, sum by value
-                self._sum += val
-            return self._sum
-
-        super().__init__(foo=foo, foo_kwargs=None, name='Sum', inputs=1)
-
-        node._downstream.append((self, 0))
-        self._upstream.append(node)
+    node._downstream.append((ret, 0))
+    ret._upstream.append(node)
+    return ret
 
 
-class Average(Node):
+def Average(node):
     '''Node to take the running average
 
     If stream type is iterable, will do (average + sum(input))/(count+len(input)).
     If input stream type is not iterable, will do (average + input)/count
     '''
+    def foo(val):
+        try:
+            # iterable, sum with sum function
+            iter(val)
+            ret._sum += sum(val)
+            ret._count += len(val)
+        except TypeError:
+            # not iterable, sum by value
+            ret._sum += val
+            ret._count += 1
+        return ret._sum / ret._count if ret._count > 0 else float('nan')
 
-    def __init__(self, node):
-        self._sum = 0
-        self._count = 0
+    ret = Node(foo=foo, foo_kwargs=None, name='Average', inputs=1)
+    ret._sum = 0
+    ret._count = 0
 
-        def foo(val):
-            try:
-                # iterable, sum with sum function
-                iter(val)
-                self._sum += sum(val)
-                self._count += len(val)
-            except TypeError:
-                # not iterable, sum by value
-                self._sum += val
-                self._count += 1
-            return self._sum / self._count if self._count > 0 else float('nan')
-
-        super().__init__(foo=foo, foo_kwargs=None, name='Average', inputs=1)
-
-        node._downstream.append((self, 0))
-        self._upstream.append(node)
+    node._downstream.append((ret, 0))
+    ret._upstream.append(node)
+    return ret
 
 
 Node.rollingCount = Count
