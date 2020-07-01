28887f40382815f48862cf7a2541c32b1e75e51e
milen <m.ferev18@gmail.com>
2020-07-01 23:19:05 +0300

    updated http output, added http output test

---
+++ tributary/tests/streaming/output/test_http_streaming.py
@@ -0,0 +1,13 @@
+import tributary.streaming as ts
+
+
+class TestHttp:
+    def test_http(self):
+        def foo():
+            yield 'x'
+            yield 'y'
+            yield 'z'
+
+        out = ts.HTTPSink(ts.Foo(foo), url='http://localhost:8080')
+        assert len(ts.run(out)) == 3
+
